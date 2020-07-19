718cd1ffa9b6c5dc98a6acff3e25df5ff7c839dd
Harsh Gupta <harshgupta951114@gmail.com>
2020-07-20 02:31:50 +0530

    Updates

---
+++ precache-manifest.adb89ca59466ba7bac5699b55a5b580a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1b0a0f31162613c4a351e43fcdfa8d",
+    "url": "/chat-application-ui/index.html"
+  },
+  {
+    "revision": "795d2e067f7858e2b9e1",
+    "url": "/chat-application-ui/static/css/main.acc2fef4.chunk.css"
+  },
+  {
+    "revision": "a81c90d6e78980b3a276",
+    "url": "/chat-application-ui/static/js/2.6ad65384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat-application-ui/static/js/2.6ad65384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795d2e067f7858e2b9e1",
+    "url": "/chat-application-ui/static/js/main.1d6bd179.chunk.js"
+  },
+  {
+    "revision": "577549b92fff3a1c6935",
+    "url": "/chat-application-ui/static/js/runtime-main.dae9aa61.js"
+  }
+]);
